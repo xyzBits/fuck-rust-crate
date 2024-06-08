a6ba36b52845d2988ebfc873a4ea9d7a167b3148
xyzBits <lidf077@gmail.com>
2024-06-08 11:49:32 +0800

    rust cheatsheet

---
+++ cheatsheet/hello-world/src/bin/cheat_02.rs
@@ -0,0 +1,70 @@
+fn main() {}
+
+#[allow(dead_code)]
+#[allow(unused)]
+#[cfg(test)]
+mod tests {
+    use std::sync::mpsc::channel;
+    use std::sync::Mutex;
+    use std::thread;
+
+    #[test]
+    fn test_send_value_to_thead() {
+        let (sender, receiver) = channel();
+
+        let handle = thread::spawn(move || {
+            let msg = receiver.recv().unwrap();
+            println!("msg = {msg}");
+        });
+
+        sender.send("hello bitch").unwrap();
+
+        handle.join().unwrap();
+    }
+
+    #[derive(Debug)]
+    struct Person {
+        name: String,
+        age: u32,
+    }
+
+    #[test]
+    fn test_sort_by_property() {
+        let mut persons = vec![
+            Person { name: "hello".to_string(), age: 863 },
+            Person { name: "world".to_string(), age: 99 },
+            Person { name: "bitch".to_string(), age: 73 },
+        ];
+
+        persons.sort_by(|a, b| (&a.age).cmp(&b.age));
+
+        println!("{:?}", persons);
+
+        persons.sort_by_key(|item| item.age);
+        println!("{:?}", persons);
+    }
+
+    #[test]
+    fn test_execute_1000_task() {
+        let handles = (0..1000).map(|i| {
+            thread::spawn(move || {})
+        })
+            .collect::<Vec<_>>();
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        // (0..100).into_iter().for_each(|| {});
+    }
+
+    #[test]
+    fn test_update_variable_atomically() {
+        let data = 5;
+        let  lock = Mutex::new(data);
+        let mut guard = lock.lock().unwrap();
+        *guard = 43;
+
+        println!("{:?}", data);
+    }
+}
