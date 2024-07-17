9276aca14993e8720afcf80e052c598b8c48ceaa
xyzBits <lidf077@gmail.com>
2024-07-17 11:36:13 +0800

    add new api

--- bitcoin/blockchain-rust/src/blockchain.rs
+++ bitcoin/blockchain-rust/src/blockchain.rs
@@ -1,3 +1,116 @@
+use std::collections::HashMap;
+use std::num::ParseFloatError;
+use std::sync::{Arc, RwLock};
+use sled::{Db, Tree};
+use crate::block::Block;
+use crate::transaction::{Transaction, TXOutput};
+
+const TIP_BLOCK_HASH_KEY: &str = "tip_block_hash";
+const BLOCK_TREE: &str = "blocks";
+
+
+#[derive(Clone)]
 pub struct Blockchain {
+    tip_hash: Arc<RwLock<String>>, // hash of last block
+    db: Db,
 
 }
+
+
+impl Blockchain {
+
+    /// 创建新的区块链
+    pub fn create_blockchain(genesis_address: &str) -> Blockchain {
+        todo!()
+    }
+
+    pub fn update_blocks_tree(blocks_tree: &Tree, block: &Block) {
+        todo!()
+    }
+
+    /// 创建区块链实例
+    pub fn new_blockchain() -> Blockchain {
+        todo!()
+    }
+
+    pub fn get_db(&self) -> &Db {
+        &self.db
+    }
+
+    pub fn get_tip_hash(&self) -> String {
+        todo!()
+    }
+
+    pub fn set_tip_hash(&self, new_tip_hash: &str) {
+        todo!()
+    }
+
+
+    /// 挖矿新区块
+    pub fn mine_block(&self, transaction: &[Transaction]) -> Block {
+        todo!()
+    }
+
+
+    pub fn iterator(&self) -> BlockchainIterator {
+        todo!()
+    }
+
+    /// 查找所有未花费的交易输出  (k -> txid_hex, v -> Vec<TXOutput>)
+    pub fn find_utxo(&self) -> HashMap<String, Vec<TXOutput>> {
+        todo!()
+    }
+
+
+    /// 从区块链中查找交易
+    pub fn find_transaction(&self, txid: &[u8]) -> Option<Transaction> {
+        todo!()
+    }
+
+    pub fn add_block(&self, block: &Block) {
+        todo!()
+    }
+
+
+    /// 获取最新区块在链中的高度
+    pub fn get_best_height(&self) -> usize {
+        todo!()
+    }
+
+
+    // 返回链中所有区块的哈希列表
+    pub fn get_block_hashed(&self) -> Vec<Vec<u8>> {
+        todo!()
+    }
+
+}
+
+
+pub struct BlockchainIterator {
+    db: Db,
+    current_hash: String,
+}
+
+
+impl BlockchainIterator {
+    pub fn new(tip_hash: String, db: Db) -> BlockchainIterator {
+        todo!()
+    }
+
+    pub fn next(&mut self) -> Option<Block> {
+        todo!()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- bitcoin/blockchain-rust/src/utxo_set.rs
+++ bitcoin/blockchain-rust/src/utxo_set.rs
@@ -1,3 +1,43 @@
+use std::collections::HashMap;
+use serde::de::value::I32Deserializer;
+use serde::forward_to_deserialize_any;
+use crate::blockchain::Blockchain;
+use crate::transaction::TXOutput;
+
+const UTXO_TREE: &str = "chainstate";
+
+/// UTXO 集合
 pub struct UTXOSet {
+    blockchain: Blockchain,
+}
+
+impl UTXOSet {
+
+    /// 创建 UTXO 集合
+    pub fn new(blockchain: Blockchain) -> UTXOSet {
+        todo!()
+    }
+
+    pub fn get_blockchain(&self) -> &Blockchain {
+        todo!()
+    }
+
+    /// 找到未花费的输出
+    pub fn find_spendable_outputs(
+        &self,
+        pub_key_hash: &[u8],
+        amount: i32
+    ) -> (i32, HashMap<String, Vec<usize>>) {
+        todo!()
+    }
+
+    /// 通过公钥哈希查找 UTXO 集合
+    pub fn find_utxo(&self, pub_key_hash: &[u8]) -> Vec<TXOutput> {
+        todo!()
+    }
+
+
+
+
 
 }
