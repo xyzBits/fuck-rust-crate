82d5c5da8568937a1d40f1fd71d53ddb74ac1e2c
xyzBits <lidf077@gmail.com>
2024-07-18 12:10:23 +0800

    utxo set

--- bitcoin/blockchain-rust/src/utxo_set.rs
+++ bitcoin/blockchain-rust/src/utxo_set.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use data_encoding::HEXLOWER;
 use crate::block::Block;
 use crate::blockchain::Blockchain;
 use crate::transaction::TXOutput;
@@ -11,22 +12,43 @@ pub struct UTXOSet {
 }
 
 impl UTXOSet {
-
     /// 创建 UTXO 集合
     pub fn new(blockchain: Blockchain) -> UTXOSet {
-        todo!()
+        UTXOSet {
+            blockchain
+        }
     }
 
     pub fn get_blockchain(&self) -> &Blockchain {
-        todo!()
+        &self.blockchain
     }
 
     /// 找到未花费的输出
     pub fn find_spendable_outputs(
         &self,
         pub_key_hash: &[u8],
-        amount: i32
+        amount: i32,
     ) -> (i32, HashMap<String, Vec<usize>>) {
+        let mut unspent_outputs: HashMap<String, Vec<usize>> = HashMap::new();
+        let mut accmulated = 0;
+
+        let db = self.blockchain.get_db();
+        let utxo_tree = db.open_tree(UTXO_TREE).unwrap();
+
+        for item in utxo_tree.iter() {
+            let (k, v) = item.unwrap();
+            let txid_hex = HEXLOWER.encode(k.to_vec().as_slice());
+            let outs: Vec<TXOutput> = bincode::deserialize(v.to_vec().as_slice())
+                .expect("unable to deserialize TXOutput");
+
+            for (idx, out) in outs.iter().enumerate() {
+
+            }
+
+
+        }
+
+
         todo!()
     }
 
@@ -42,17 +64,11 @@ impl UTXOSet {
 
 
     /// 重建 UTXO 集合
-    pub fn reindex(&self) {
-
-    }
+    pub fn reindex(&self) {}
 
 
     /// 使用来自区块的交易更新 UTXO 集
     pub fn update(&self, block: &Block) {
         todo!()
     }
-
-
-
-
 }
