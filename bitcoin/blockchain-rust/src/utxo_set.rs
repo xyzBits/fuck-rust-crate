3b0999ac3fee78bedccb0165ac6862e8a2bae31b
xyzBits <lidf077@gmail.com>
2024-07-19 11:31:05 +0800

    impl utxo set

--- bitcoin/blockchain-rust/src/utxo_set.rs
+++ bitcoin/blockchain-rust/src/utxo_set.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
+
 use data_encoding::HEXLOWER;
+
 use crate::block::Block;
 use crate::blockchain::Blockchain;
 use crate::transaction::TXOutput;
@@ -30,7 +32,7 @@ impl UTXOSet {
         amount: i32,
     ) -> (i32, HashMap<String, Vec<usize>>) {
         let mut unspent_outputs: HashMap<String, Vec<usize>> = HashMap::new();
-        let mut accmulated = 0;
+        let mut accumulated = 0;
 
         let db = self.blockchain.get_db();
         let utxo_tree = db.open_tree(UTXO_TREE).unwrap();
@@ -42,29 +44,71 @@ impl UTXOSet {
                 .expect("unable to deserialize TXOutput");
 
             for (idx, out) in outs.iter().enumerate() {
-
+                if out.is_locked_with_key(pub_key_hash) && accumulated < amount {
+                    accumulated += out.get_value();
+                    if unspent_outputs.contains_key(txid_hex.as_str()) {
+                        unspent_outputs
+                            .get_mut(txid_hex.as_str())
+                            .unwrap()
+                            .push(idx);
+                    } else {
+                        unspent_outputs.insert(txid_hex.clone(), vec![idx]);
+                    }
+                }
             }
-
-
         }
 
 
-        todo!()
+        (accumulated, unspent_outputs)
     }
 
     /// 通过公钥哈希查找 UTXO 集合
     pub fn find_utxo(&self, pub_key_hash: &[u8]) -> Vec<TXOutput> {
-        todo!()
+        let db = self.blockchain.get_db();
+        let utxo_tree = db.open_tree(UTXO_TREE).unwrap();
+        let mut utxos = vec![];
+        for item in utxo_tree.iter() {
+            let (_, v) = item.unwrap();
+            let outs: Vec<TXOutput> = bincode::deserialize(v.to_vec().as_slice())
+                .expect("Unable to deserialize TXOutput");
+
+            for out in outs.iter() {
+                if out.is_locked_with_key(pub_key_hash) {
+                    utxos.push(out.clone());
+                }
+            }
+        }
+
+        utxos
     }
 
     /// 统计 UTXO 集合中的交易数量
     pub fn count_transactions(&self) -> i32 {
-        todo!()
+        let db = self.blockchain.get_db();
+        let utxo_tree = db.open_tree(UTXO_TREE).unwrap();
+        let mut counter = 0;
+        for _ in utxo_tree.iter() {
+            counter += 1;
+        }
+
+        counter
     }
 
 
     /// 重建 UTXO 集合
-    pub fn reindex(&self) {}
+    pub fn reindex(&self) {
+        let db = self.blockchain.get_db();
+        let utxo_tree = db.open_tree(UTXO_TREE).unwrap();
+        let _ = utxo_tree.clear().unwrap();
+
+        let utxo_map = self.blockchain.find_utxo();
+        for (txid_hex, outs) in &utxo_map {
+            let txid = HEXLOWER.decode(txid_hex.as_bytes()).unwrap();
+            let value = bincode::serialize(outs).unwrap();
+
+            let _ = utxo_tree.insert(txid.as_slice(), value).unwrap();
+        }
+    }
 
 
     /// 使用来自区块的交易更新 UTXO 集
