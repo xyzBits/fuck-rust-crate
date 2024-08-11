93e2a9b8bae4d5e03b5d7b862ac7b39a502b0b0d
xyzBits <lidf077@gmail.com>
2024-08-11 10:09:45 +0800

    update gitignore

--- axum/hello-world/src/main.rs
+++ axum/hello-world/src/main.rs
@@ -1,21 +1,34 @@
+use std::sync::Arc;
+
 use axum::{
-    http::StatusCode,
-    routing::{get, post},
-    Json, Router,
+    extract::State, http::StatusCode, routing::{get, post}, Json, Router
 };
 use serde::{Deserialize, Serialize};
+
+
+
+#[derive(Debug)]
+struct HttpServeState {
+    password: String,
+}
+
+
 
 #[tokio::main]
 async fn main() {
     // initialize tracing
     tracing_subscriber::fmt::init();
+    let state = HttpServeState {
+        password: "hello axum".to_string(),
+    };
 
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(root))
         // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/users", post(create_user))
+        .with_state(Arc::new(state));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -30,8 +43,12 @@ async fn root() -> &'static str {
 async fn create_user(
     // this argument tells axum to parse the request body
     // as JSON into a `CreateUser` type
+    State(state): State<HttpServeState>,
     Json(payload): Json<CreateUser>,
 ) -> (StatusCode, Json<User>) {
+
+    println!("{:?}", state);
+
     // insert your application logic here
     let user = User {
         id: 1337,
