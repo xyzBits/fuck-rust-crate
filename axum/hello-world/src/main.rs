16d758d685b318faae0c799b8f21ce124dd7f838
xyzBits <lidf077@gmail.com>
2024-08-11 08:48:35 +0800

    hello axum

--- axum/hello-world/src/main.rs
+++ axum/hello-world/src/main.rs
@@ -1,25 +1,49 @@
-use axum::response::Html;
-use axum::Router;
-use axum::routing::get;
+use axum::http::StatusCode;
+use axum::routing::{get, post};
+use axum::{Json, Router};
+use serde::Deserialize;
+use tracing::info;
 
 #[tokio::main]
 async fn main() {
+    // 初始化日志
+    tracing_subscriber::fmt::init();
+
     // build our application with a route
-    let app = Router::new().route("/hello-world", get(handler));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
 
     // run it
-    let listener =
-        tokio::net::TcpListener::bind("127.0.0.1:3000")
-            .await
-            .unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
 
-    println!("listening on {}", listener.local_addr().unwrap());
+    info!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
-
-    println!("Hello, world!");
 }
 
+async fn root() -> &'static str {
+    "Hello, World!"
+}
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
+    let user = User {
+        id: 1377,
+        username: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+struct User {
+    id: i64,
+    username: String,
+}
+
+// 必须要在 cargo.toml  中加上 derive 才能使用这个宏
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
 }
