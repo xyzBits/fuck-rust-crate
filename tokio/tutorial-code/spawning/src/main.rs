b3c2752fff1f7db5a3c2344dceb78b8e91784a76
xyzBits <lidf077@gmail.com>
2024-09-12 15:19:50 +0800

    tokio spawn

--- tokio/tutorial-code/spawning/src/main.rs
+++ tokio/tutorial-code/spawning/src/main.rs
@@ -1,4 +1,5 @@
-use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use mini_redis::{Command, Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -21,13 +22,29 @@ async fn process(socket: TcpStream) {
     // 连接可以让我们通过 字节流，读写 redis 的 frame，连接类型被 mini-redis定义
     let mut connection = Connection::new(socket);
 
+    // 存储数据的 hash map
+    // todo 有一个问题，值 不能够在连接中共享，如果另外一个 socket 尝试通过 GET 获取 另一个连接设置的值，将不会找到任何东西
+    let mut db = HashMap::new();
+
     if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+        let response = match Command::from_frame(frame).unwrap() {
+            Command::Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().clone());
+                Frame::Simple("OK".to_string())
+            }
 
-        // 响应一个错误
-        let response = Frame::Error("unimplemented".to_string());
+            Command::Get(cmd) => {
+                if let Some(key) = db.get(cmd.key()) {
+                    Frame::Bulk(key.clone())
+                } else {
+                    Frame::Null
+                }
+            }
 
-        let response = Frame::Simple("fuck you".to_string());
+            cmd => panic!("unimplemented command: {:?}", cmd),
+        };
+
+        // 写入响应到客户端
         connection.write_frame(&response).await.unwrap();
     }
 }
