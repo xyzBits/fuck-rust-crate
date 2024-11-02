4225d37c2115a788a0203d792ac802311fe980e7
xyzBits <lidf077@gmail.com>
2024-11-02 21:21:58 +0800

    add example

--- cheatsheet/programming-rust/src/chapter_05/mod.rs
+++ cheatsheet/programming-rust/src/chapter_05/mod.rs
@@ -133,3 +133,75 @@ fn test_implicit_deref() {
     let iter = (&data).into_iter();
     println!("{:?}", data);
 }
+
+#[test]
+fn test_ref_compare() {
+    let x = 5;
+    let r = &x;
+    let new_r = &r;
+    // assert!(r >= new_r);
+}
+
+#[test]
+fn test_01() {
+    let x = 10;
+    let y = 20;
+    let mut r = &x;
+    let b = true;
+    if b {
+        r = &y;
+    }
+
+    assert!(*r == 10 || *r == 20);
+}
+
+#[test]
+fn test_02() {
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    let point = Point { x: 1000, y: 729 };
+    let r = &point;
+    let rr: &&Point = &r;
+    let rrr = &rr;
+    // . 运算符寻找目标时，需要解引用多次，在rrr 类型的引导下，实际上进行了3次解引用才得到了 point 的值，然后获得它的 y 字段
+    assert_eq!(rrr.y, 729);
+}
+
+#[test]
+fn test_03() {
+    let x = 10;
+    let y = 10;
+
+    let rx = &x;
+    let ry = &y;
+
+    let rrx = &rx;
+    let rry = &ry;
+
+    // 类似于 . 运算符，比较运算符可以看穿任意数量的 引用嵌套
+    // 尽管 rrx rry 指向不同的值，但因为 == 运算符会解除所有的引用然后对最终的值 x y 进行比较，
+    // 这几乎总是你想要的行为，尤其是编写泛型函数时，如果你实际上是想知道两个引用是否指向相同的内存位置，可以使用 std::ptr::eq，它会按照地址比较引用
+    assert!(rrx <= rry);
+    assert_eq!(rrx, rry);
+
+    assert_eq!(rx, ry); // 指向的对象相等
+    assert!(!std::ptr::eq(rrx, rry)); // 但是地址不同
+
+    // 注意比较运算符的操作数的类型必须完全相同，包括引用
+    // assert!(rx == rrx); // error 类型不匹配，&i32 &&i32
+    assert_eq!(rx, *rrx);
+}
+
+fn factorial(n: usize) -> usize {
+    (1..(n + 1)).product()
+}
+
+#[test]
+fn test_factorial() {
+    let r = &factorial(6);
+    // 算术运算符可以看穿一层引用
+    assert_eq!(r + &1009, 1729);
+}
