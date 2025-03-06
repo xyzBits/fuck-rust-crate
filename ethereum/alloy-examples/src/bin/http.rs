c0e70ee7b962b85eb88a3652f37dc2033e8b4bcf
xyzBits <lidf077@gmail.com>
2025-03-06 18:22:31 +0800

    add examples

---
+++ ethereum/alloy-examples/src/bin/http.rs
@@ -0,0 +1,15 @@
+//! 使用 http 接口获取最 latest block number
+use alloy::providers::{Provider, ProviderBuilder};
+use eyre::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let rpc_url = "https://eth.merkle.io".parse()?;
+
+    let provider = ProviderBuilder::new().on_http(rpc_url);
+
+    let latest_block = provider.get_block_number().await?;
+
+    println!("Latest block number: {}", latest_block);
+    Ok(())
+}
